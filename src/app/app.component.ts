aeca54868a2308e23025945891eb67cbd08928dc
KostyaTretyak <ktretiak.in.ua@gmail.com>
2020-08-31 17:10:52 +0300

    Init @ng-stack/forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@ng-stack/forms';
+// import { FormGroup, FormBuilder } from '@angular/forms';
+
+interface Model {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ng-stack-test-v10';
+export class AppComponent implements OnInit {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      id: {value: 1, disabled: false},
+      name: [{value: 'Kostia', disabled: false}],
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgStackFormsModule } from '@ng-stack/forms';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgStackFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
